92acabf7387f7c5983cf7a99ac604fc8cbd7d1c4
Vasiliy Shubko <flit77@gmail.com>
2017-08-27 01:44:19 +0200

    contact page updated

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -4,8 +4,7 @@ export default class About extends PureComponent {
   render () {
     return (
       <div className="inner cover">
-        <h1 className="cover-heading">Any Questions?</h1>
-        <p className="lead">Don't hesitate to contact me: zaza.samuele@gmail.com</p>
+        <h1 className="cover-heading">Contact Form</h1>
       </div>
     );
   }
