32c457256c48832677e7efa0caf446b3d44dfbdb
Vasiliy Shubko <flit77@gmail.com>
2017-08-28 00:32:12 +0200

    redux temporarily removed from addGameContainer

--- client/src/containers/AddGameContainer.jsx
+++ client/src/containers/AddGameContainer.jsx
@@ -1,46 +1,62 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import { hashHistory } from 'react-router';
 import { Form } from '../components';
-import * as gamesActionCreators from '../actions/games';
-import * as filestackActionCreators from '../actions/filestack';
 
-class AddGameContainer extends Component {
+export default class AddGameContainer extends Component {
   constructor (props) {
     super(props);
+    // Initial state
+    this.state = { newGame: {}};
+    // Bind this (context) to the functions to be passed down to the children components
     this.submit = this.submit.bind(this);
     this.uploadPicture = this.uploadPicture.bind(this);
+    this.setGame = this.setGame.bind(this);
   }
-  submit (event) {
-    event.preventDefault();
-    this.props.gamesActions.postGame();
+  submit () {
+    // We create the newGame object to be posted to the server
+    const newGame = Object.assign({}, { picture: $('#picture').attr('src') }, this.state.newGame);
+    fetch('http://localhost:8080/games', {
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }),
+      method: 'POST',
+      body: JSON.stringify(newGame)
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.message);
+      // We go back to the games list view
+      hashHistory.push('/games');
+    });
   }
   uploadPicture () {
-    console.log(`here is uploading picture`);
-  }
-  render () {
-    const { picture } = this.props;
-    return (
-      <Form
-        handleSubmit={this.submit}
-        picture={picture}
-        uploadPicture={this.uploadPicture}
-      />
+    filepicker.pick (
+      {
+        mimetype: 'image/*', // Cannot upload other files but images
+        container: 'modal',
+        services: ['COMPUTER', 'FACEBOOK', 'INSTAGRAM', 'URL', 'IMGUR', 'PICASA'],
+        openTo: 'COMPUTER' // First choice to upload files from
+      },
+      function (Blob) {
+        console.log(JSON.stringify(Blob));
+        $('#picture').attr('src', Blob.url);
+      },
+      function (FPError) {
+        console.log(FPError.toString());
+      }
     );
   }
-}
-
-function mapStateToProps (state) {
-  return {
-    picture: state.getIn(['filestack', 'url'], '')
+  // We make sure to keep the state up-to-date to the latest input values
+  setGame () {
+    const newGame = {
+      name: document.getElementById('name').value,
+      description: document.getElementById('description').value,
+      year: document.getElementById('year').value,
+      picture: $('#picture').attr('src')
+    };
+    this.setState({ newGame });
+  }
+  render () {
+    return <Form submit={this.submit} uploadPicture={this.uploadPicture} setGame={this.setGame} />
   }
 }
-
-function mapDispatchToProps (dispatch) {
-  return {
-    gamesActions: bindActionCreators(gamesActionCreators, dispatch),
-    filestackActions: bindActionCreators(filestackActionCreators, dispatch)
-  };
-}
-export default connect(mapStateToProps, mapDispatchToProps)(AddGameContainer);
