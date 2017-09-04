b19124535fb7b1e8ba1fab3d090f1ca8839e731e
Vasiliy Shubko <flit77@gmail.com>
2017-09-04 20:53:19 +0200

    form switched to redux

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 // We import the combineReducers function
 import { combineReducers } from 'redux-immutable';
+import { reducer as form } from 'redux-form/immutable';
 // Import our reducers function from here
 import games from './games';
 
 // combineReducers merges them all!
 export default combineReducers({
-  games
+  games,
+  form
 });
