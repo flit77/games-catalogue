e4707bea39aacf925c8bf4b732e94e7d7fd630cb
Vasiliy Shubko <flit77@gmail.com>
2017-09-07 23:37:48 +0200

    store update with router middleware

--- client/src/store.js
+++ client/src/store.js
@@ -1,14 +1,20 @@
 // We import Redux and Redux-saga dependencies
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from './sagas';
 import reducer from './reducers';
+import { routerMiddleware } from 'react-router-redux';
+import { hashHistory } from 'react-router';
 
 // The function in charge of creating and returning the store of the app
 const configureStore = () => {
   const sagaMiddleware = createSagaMiddleware();
   // The store is created with a reducer parameter and the saga middleware
-  const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+  const routeMiddleware = routerMiddleware(hashHistory);
+  const store = createStore(
+    reducer,
+    applyMiddleware(sagaMiddleware, routeMiddleware)
+  );
   // rootSaga starts all the sagas in parallel
   sagaMiddleware.run(rootSaga);
 
