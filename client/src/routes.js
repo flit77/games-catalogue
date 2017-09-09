9617f45a9f10aef87fe9310fcbb0d0ec1ff356e9
Vasiliy Shubko <flit77@gmail.com>
2017-09-09 18:47:55 +0200

    redux history added to router

--- client/src/routes.js
+++ client/src/routes.js
@@ -4,14 +4,21 @@ import configureStore from './store';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import { AddGameContainer, GamesContainer } from './containers';
 import { Home, Archive, Welcome, About, Contact } from './components';
+import { syncHistoryWithStore } from 'react-router-redux';
 
 // Call the configureStore function previously exported
 const store = configureStore();
 
+const history = syncHistoryWithStore(hashHistory, store, {
+  selectLocationState(state) {
+    return state.get('routing').toObject();
+  }
+});
+
 // Provider wraps our root component
 const routes = (
   <Provider store={store}>
-    <Router history={hashHistory}>
+    <Router history={history}>
       <Route path="/" component={Home}>
         <IndexRoute component={Welcome} />
         <Route path="/about" component={About} />
