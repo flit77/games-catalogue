f09b4ff12e2131e0f59eeedc28a1d98116c14664
Vasiliy Shubko <flit77@gmail.com>
2017-09-09 21:13:07 +0200

    reducers updated with dispatching auth

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,15 +1,14 @@
-// We import the combineReducers function
 import { combineReducers } from 'redux-immutable';
 import { reducer as form } from 'redux-form/immutable';
-// Import our reducers function from here
 import games from './games';
 import filestack from './filestack';
+import auth from './auth';
 import routing from './routing';
 
-// combineReducers merges them all!
 export default combineReducers({
   games,
   form,
   filestack,
+  auth,
   routing
 });
